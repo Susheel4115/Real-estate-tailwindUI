e171a2a58f506d35fbda64e506a2c1734b4339f4
Susheel Kumar <74233895+Susheel4115@users.noreply.github.com>
2022-06-25 23:19:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,18 +7,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Property from "./components/Property";
 export default function App() {
   return (
-    // <Router>
-    //   <div className="App">
-    //     <Routes>
-    //       <Route exact path={"/"} element={<Login />}></Route>
-    //       <Route exact path={"/login"} element={<Login />}></Route>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path={"/"} element={<Login />}></Route>
+          <Route exact path={"/login"} element={<Login />}></Route>
 
-    //       <Route exact path={"/signup"} element={<Signup />}></Route>
-    //     </Routes>
-    //   </div>
-    // </Router>
-    <div className="m-10 max-h-screen">
-      <Property />
-    </div>
+          <Route exact path={"/signup"} element={<Signup />}></Route>
+        </Routes>
+      </div>
+    </Router>
+//     <div className="m-10 max-h-screen">
+//       <Property />
+//     </div>
   );
 }
